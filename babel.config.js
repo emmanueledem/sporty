de3141acdcc9292aa0055a1c9c3071419c1cabe8
Emmanuel okon <emmanuelokon591@gmail.com>
2024-10-06 23:28:07 +0100

    revoked api key

--- babel.config.js
+++ babel.config.js
@@ -5,3 +5,23 @@ module.exports = function (api) {
     plugins: ["nativewind/babel"],
   };
 };
+
+// module.exports = function (api) {
+//   api.cache(true);
+//   return {
+//     presets: ["module:metro-react-native-babel-preset"],
+//     plugins: [
+//       [
+//         "module:react-native-dotenv",
+//         {
+//           moduleName: "@env",
+//           path: ".env",
+//           blacklist: null,
+//           whitelist: null,
+//           safe: false,
+//           allowUndefined: true,
+//         },
+//       ],
+//     ],
+//   };
+// };
